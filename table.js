9728c2c2f2ff61969883196f9b2a2592e97c6aba
St3ph4ne <spodar.s@protonmail.com>
2021-12-06 15:56:10 +0000

    compagnie assurance 1.0

--- table.js
+++ table.js
@@ -1,130 +1,47 @@
-let leap = false;
-let longMonth = false;
-
-// let jour;
-// let mois;
-// let annee;
-
-var jour_lendemain;
-var mois_lendemain;
-var annee_lendemain;
-var jour_veille;
-var mois_veille;
-var annee_veille;
-
 // enregistrement du jour de départ:
-jour = parseInt(prompt("quel jour?"));
-mois = parseInt(prompt("quel mois?"));
-annee = parseInt(prompt("quelle année?"));
+let age = parseInt(prompt("Votre âge svp?"));
+let seniority = parseInt(prompt("Depuis combien d'années avez-vous le permis ?"));
+let rugged = parseInt(prompt("Combien avez-vous déjà eu d'accident.s ?"));
+let assured = parseInt(prompt("Depuis combien d'années êtes-vous assuré.e ?"));
+let tarif;
+let bonus = false;
 
-// affichage du jour de départ:
-console.log(jour);
-console.log(mois);
-console.log(annee);
+const table = ["Votre tarif est A", "Votre tarif est B", "Votre tarif est C", "Votre tarif est D", "Nous regrettons de ne pouvoir vous assurer."];
 
-// vérification des mois en 31 jours:
-if(mois === 1 || mois === 3 || mois === 5 || mois === 7 || mois === 8 || mois === 10 || mois === 12){
-  longMonth = true;
-  alert("Le mois compte 31 jours");
+if(assured > 1){
+  bonus = true;
 }
 
-// vérification des années bissextiles:
-if((annee % 4 === 0 && (annee % 100) != 0) || (annee % 400) === 0 ){
-  leap = true;
-  alert("L'année est bissextile.");
-}
 
-// vérification de la validité de la date de départ:
-if(jour < 1 || jour > 31 || mois < 1 || mois > 12 || (mois === 2 && jour > 29) || annee < 1582){
-  alert("date erronée !");
-} else {
-  
-  // Affichage de la date du lendemain:
-  
-  // cas du 28 février:
-  if(jour === 28 && mois === 2 && leap === false){
-    jour_lendemain = 1;
-    mois_lendemain = mois +1;
-    annee_lendemain = annee;
-  
-    // cas du 30 d'un mois en 30 jours:
-  } else if(jour === 30 && longMonth === false){
-    jour_lendemain = 1;
-    mois_lendemain = mois + 1;
-    annee_lendemain = annee;
+if(age < 25 && seniority < 2){
+  if(rugged > 0){
+    tarif = 4;
+  } else tarif = 3;
 
-    // cas du 31 décembre:
-  } else if(jour === 31 && mois === 12){
-    jour_lendemain = 1;
-    mois_lendemain = 1;
-    annee_lendemain = annee + 1;
+}else if((age < 25 && seniority > 2) || (age >= 25 && seniority < 2)) {
+  if(rugged === 0){
+    tarif = 2;
+  } else if(rugged === 1){
+    tarif = 3;
+  } else if(rugged > 1){
+    tarif = 4;
+  }
 
-    // cas du 31 des autres mois:
-  } else if(jour === 31){
-    jour_lendemain = 1;
-    mois_lendemain = mois + 1;
-    annee_lendemain = annee;
-
-    // les autres cas:
-  } else {
-    jour_lendemain =  jour + 1;
-    mois_lendemain = mois;
-    annee_lendemain = annee;
+}else if(age >= 25 && seniority > 2){
+  if(rugged === 0){
+    tarif = 1;
+  }else if(rugged === 1){
+    tarif = 2;
+  }else if(rugged === 2){
+    tarif = 3;
+  }else if(rugged > 2){
+    tarif = 4;
   }
   
-  console.log(jour_lendemain);
-  console.log(mois_lendemain);
-  console.log(annee_lendemain);
-  
-  
-  // Pour l'affichage de la date de la veille:
-  let longLastMonth = false;
-  
-  // vérification si le mois précédent est en 31 jours:
-  if( mois === 1 || mois === 2 || mois === 4 || mois === 6 || mois === 8 || mois === 9 || mois === 11){
-    longLastMonth = true;
-  };
-  
-  // cas du 1er janvier:
-  if(jour === 1 && mois === 1){
-    jour_veille = 31;
-    mois_veille = 12;
-    annee_veille = annee - 1;
-  
-    //cas du 1er mars d'une année bissextile:
-  } else if(jour === 1 && mois === 3 && leap === true){
-    jour_veille = 29;
-    mois_veille = mois - 1;
-    annee_veille = annee;
-  
-    // cas du 1er mars d'une année non-bissextile:
-  } else if(jour === 1 && mois === 3 && leap === false){
-    jour_veille = 28;
-    mois_veille = mois - 1;
-    annee_veille = annee;
-  
-    // cas du 1er quand le mois précédent est en 31 jours:
-  } else if(jour === 1 && (longLastMonth)){
-    jour_veille = 31;
-    mois_veille = mois - 1;
-    annee_veille = annee;
-  
-    // cas du 1er quand le mois précédent est en 30 jours:
-  } else if(jour === 1 && (!longLastMonth)){
-    jour_veille = 30;
-    mois_veille = mois - 1;
-    annee_veille = annee;
-
-    // les autres cas:
-  } else {
-    jour_veille = jour - 1;
-    mois_veille = mois;
-    annee_veille = annee;
-  }
-  
-  console.log(jour_veille);
-  console.log(mois_veille);
-  console.log(annee_veille);
-
 }
 
+if(bonus){
+  if(tarif > 0) tarif--;
+}
+
+alert(table[tarif]);
